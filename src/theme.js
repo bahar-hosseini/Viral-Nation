dd655e1b06429e84cb0f1e35e79f2c49aa733ea1
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-06-21 16:03:45 -0400

    create theme

---
+++ src/theme.js
@@ -0,0 +1,43 @@
+import { createTheme } from "@mui/material/styles";
+
+// Dark Mode Theme
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#404040",
+            light:"#181A1C",
+            dark:"#121212"
+        },
+        secondary: {
+            main: "##3B94ED",
+            
+        },
+        background: {
+            default: "#1E1E1E",
+            paper: "#181A1C",
+        },
+    },
+});
+
+// Light Mode Theme
+const lightTheme = createTheme({
+    palette: {
+        mode: "light",
+        primary: {
+            main: "#2196f3",
+            light:"#FCFCFD",
+            dark:"#F6F6F6"
+
+        },
+        secondary: {
+            main: "#3DACFF",
+        },
+        background: {
+            default: "#ffffff",
+            paper: "#F6F6F6",
+        },
+    },
+});
+
+export { darkTheme, lightTheme };
