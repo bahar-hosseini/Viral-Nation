1fae746ece96c2fc318a40c7793ce72721c8839b
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-06-23 15:53:43 -0400

    Add create Profile feature

--- src/components/CreateProfile.js
+++ src/components/CreateProfile.js
@@ -1,85 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
 import Dialog from "@mui/material/Dialog";
 import {
   DialogTitle,
   Divider,
-  Switch,
   Box,
-  Paper,
+  Switch,
+  FormControl,
   TextField,
-  Typography,
   Button,
   Stack,
+  Typography,
 } from "@mui/material";
 
+const CREATE_PROFILE_MUTATION = gql`
+  mutation CreateProfile(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $isVerified: Boolean!
+    $imageUrl: String!
+    $description: String!
+  ) {
+    createProfile(
+      first_name: $firstName
+      last_name: $lastName
+      email: $email
+      is_verified: $isVerified
+      image_url: $imageUrl
+      description: $description
+    ) {
+      id
+      first_name
+      last_name
+      email
+      is_verified
+      image_url
+      description
+    }
+  }
+`;
+
 const CreateProfile = ({ open, setOpen }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isVerified, setIsVerified] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [createProfile] = useMutation(CREATE_PROFILE_MUTATION);
+
+  const handleCreateProfile = () => {
+    createProfile({
+      variables: {
+        firstName,
+        lastName,
+        email,
+        isVerified,
+        imageUrl,
+        description,
+      },
+    })
+      .then((response) => {
+        console.log("Profile created:", response.data.createProfile);
+        setOpen(false);
+      })
+      .catch((error) => {
+        console.error("Error creating profile:", error);
+      });
+  };
+
   return (
     <Dialog open={open} onClose={() => setOpen(false)}>
       <Box sx={{ p: 4 }}>
-        <DialogTitle>Create Profile</DialogTitle>
-        <Divider />
-        <TextField
-          sx={{ my: 2 }}
-          type="text"
-          defaultValue="https://link.to.an.image"
-          fullWidth
-          label="image link"
-          size="small"
-        />
-        <Box my={2} direction="row" justifyContent="space-around">
+        <FormControl>
+          <DialogTitle>Create Profile</DialogTitle>
+          <Divider />
           <TextField
+            sx={{ my: 2 }}
             type="text"
-            label="First Name"
-            defaultValue="Dariene"
-            size="small"
+            fullWidth
+            label="Image URL"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
           />
+          <Stack my={2} direction="row" justifyContent="space-around">
+            <TextField
+              sx={{ mr: 1 }}
+              type="text"
+              fullWidth
+              label="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+
+            <TextField
+              sx={{ ml: 1 }}
+              type="text"
+              fullWidth
+              label="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </Stack>
           <TextField
-            size="small"
-            type="text"
-            label="Last Name"
-            defaultValue="Robben"
-            sx={{
-              ml: 1,
-            }}
+            sx={{ my: 2 }}
+            type="email"
+            fullWidth
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-        </Box>
-        <TextField
-          size="small"
-          sx={{ my: 2 }}
-          type="text"
-          defaultValue="DarieneRobben@gmail.com"
-          fullWidth
-          label="Email"
-        />
-        <TextField
-          size="small"
-          sx={{ my: 2 }}
-          type="text"
-          defaultValue="Description"
-          fullWidth
-          multiline
-          rows={5}
-          label="Description"
-        />
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{ backgroundColor: "primary.dark", borderRadius: "5px", py: 1 }}
-        >
-          <Typography sx={{ mx: 2 }}>Verification</Typography>
-          <Switch color="secondary" />
-        </Stack>
-        <Divider sx={{ mt: 6 }} />
-        <Box sx={{ mt: 2 }} display="flex" justifyContent="flex-end">
-          <Button
-            color="secondary"
-            variant="contained"
-            aria-label="create profile"
+
+          <TextField
+            sx={{ my: 2 }}
+            type="text"
+            fullWidth
+            multiline
+            rows={5}
+            label="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ backgroundColor: "primary.dark", borderRadius: "5px", py: 1 }}
           >
-            Create Profile
-          </Button>
-        </Box>
+            <Typography sx={{ mx: 2 }}>Verification</Typography>
+            <Switch
+              color="secondary"
+              onChange={() => setIsVerified(!isVerified)}
+            />
+          </Stack>
+          <Divider sx={{ mt: 6 }} />
+          <Box sx={{ mt: 2 }} display="flex" justifyContent="flex-end">
+            <Button
+              color="secondary"
+              variant="contained"
+              aria-label="create profile"
+              type="submit"
+              onClick={handleCreateProfile}
+            >
+              Create Profile
+            </Button>
+          </Box>
+        </FormControl>
       </Box>
     </Dialog>
   );
